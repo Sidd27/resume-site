494fa39d757302282fb8ee3837719a30580313fd
Siddharth Pandey <pandeysiddharth27@gmail.com>
2024-08-03 22:40:23 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,270 @@
+function App() {
+  return (
+    <div className="max-w-4xl mx-auto p-6">
+      <header className="mb-8">
+        <h1 className="text-4xl font-bold text-primary">Siddharth Pandey</h1>
+        <p>
+          401, PJC Prince, AECS Layout, Singasandra, Bangalore, Karnataka -
+          560068
+        </p>
+        <p>T: 8867341449 | 9752012227</p>
+        <p>E: pandeysiddharth27@gmail.com</p>
+        <p>
+          <a
+            className="text-primary"
+            href="http://stackoverflow.com/users/2852415/siddharth-pandey"
+          >
+            StackOverflow
+          </a>{" "}
+          |
+          <a className="text-primary" href="https://github.com/Sidd27">
+            {" "}
+            GitHub
+          </a>{" "}
+          |
+          <a className="text-primary" href="https://www.npmjs.com/~sidd27">
+            {" "}
+            NPM
+          </a>
+        </p>
+      </header>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold text-primary">Objective</h2>
+        <p>
+          To secure a challenging position where I can effectively contribute my
+          skills as Software Professional & to join a progressive organization
+          to contribute to the fulfillment of the corporate goals as well as
+          achieve professional and personal growth.
+        </p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold text-primary">Summary</h2>
+        <p>
+          11+ years of experience with a strong environmental awareness,
+          excellent code writing skills, and the ability to find innovative
+          solutions on an architecture level. I can handle a team and love
+          challenging work. MEAN/MERN Stack is my forte.
+        </p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold text-primary">Experience</h2>
+
+        <div className="pl-4 border-l-4 border-primary mb-4">
+          <h3 className="text-xl font-bold">
+            Lead Software Engineer, HyperTrack
+          </h3>
+          <p className="italic">Sep 2022 - Present</p>
+          <p>
+            Job Description: To understand the project which is being assigned,
+            design the project architecture, write good quality, groom other
+            developers, with end to end product understanding. Take care of
+            site/product performance aspects while coding. Review code and
+            ensure code changes meet quality, are bug free, etc.
+            Implement/develop global solutions to help the smooth functioning of
+            development. Test the code which we have written properly as we
+            don’t separate the QA team. Deploy Code on production, Interact
+            directly with customers and resolve their issues.
+          </p>
+          <p>
+            Projects: Made Micro Frontend UI Dashboard from Scratch in React
+            using webpack module federation, Worked on existing Last Mile Fleet
+            Management Dashboard, Made design system for complete organization.
+          </p>
+          <p>
+            Technologies: React 18 with Typescript, Webpack Module Federation,
+            Zustand, Recoil, JavaScript, HTML, CSS, CSS3, Material - UI,
+            Tailwind, Radix UI. Storybook
+          </p>
+        </div>
+
+        <div className="pl-4 border-l-4 border-primary mb-4">
+          <h3 className="text-xl font-bold">
+            Associate Technical Architect, Telio
+          </h3>
+          <p className="italic">Oct 2020 - Aug 2022</p>
+          <p>
+            Job Description: To understand the project which is being assigned,
+            design the project architecture, write good quality, groom other
+            developers, with end to end product understanding. Take care of
+            site/product performance aspects while coding. Review code and
+            ensure code changes meet quality, are bug free, etc.
+            Implement/develop global solutions to help the smooth functioning of
+            development. Migration from Magento: To develop everything from
+            scratch we have to migrate each process from magento or the process
+            which is currently offline.
+          </p>
+          <p>
+            Projects: MicroFrontend UI, Order Management System, Promotions
+            Management System, Customer Management System, Access Role
+            Management, Inventory Management System, Hybrid Mobile Application,
+            Customer website and many more.
+          </p>
+          <p>
+            Technologies: React 17, Single SPA, NodeJS (Express), MongoDB,
+            JavaScript, HTML, CSS, CSS3, Material - UI.
+          </p>
+        </div>
+
+        <div className="pl-4 border-l-4 border-primary mb-4">
+          <h3 className="text-xl font-bold">UI-III, Lendingkart.com</h3>
+          <p className="italic">Apr 2018 - Aug 2020</p>
+          <p>
+            Job Description: To understand the project which is being assigned,
+            design the project architecture, write good quality, groom other
+            developers, with end to end product understanding. Take care of
+            site/product performance aspects while coding. Ensure enough logging
+            is present for debugging any use cases. Review code and ensure code
+            changes meet quality, are bug free, etc. Implement/develop global
+            solutions to help the smooth functioning of development.
+          </p>
+          <p>Accomplishments: Received 3rd Prize on Hackathon (FacXn).</p>
+          <p>Projects Handled:</p>
+          <ul className="list-disc list-inside">
+            <li>
+              Headless CMS - (Created from Scratch). A Multi theme, Vernacular
+              support Headless CMS for Landing Pages. Technologies: Angular 8,
+              NodeJS (Express), MongoDB, JavaScript, HTML, CSS, CSS3, Bootstrap.
+            </li>
+            <li>
+              Landing Pages for Loan - (Created from Scratch). A Landing Pages
+              project with multiple A/B testing mainly focused around
+              performance of the page. Technologies: NodeJS (Express), Vanilla
+              JavaScript, HTML, CSS, CSS3, Bootstrap.
+            </li>
+            <li>
+              HTML to PDF Converter - (Created from Scratch). A project which
+              converts the HTML to PDF with help of Puppeteer. Technologies:
+              NodeJS (Fastify), MongoDB.
+            </li>
+            <li>
+              Landing Pages for Health Insurance - (Created from Scratch). A
+              Landing Pages project with multiple forms mainly focused around
+              performance of the page. Technologies: Svelte (Sapper).
+            </li>
+            <li>
+              Customer Dashboard Loan - (Created from Scratch). A Dashboard
+              project with dynamic component loading with current on going loan
+              informations. Technologies: Angular 7 (Angular Material), HTML,
+              CSS, CSS3.
+            </li>
+          </ul>
+        </div>
+
+        <div className="pl-4 border-l-4 border-primary mb-4">
+          <h3 className="text-xl font-bold">
+            Senior Software Developer, Coviam.com
+          </h3>
+          <p className="italic">May 2016 - Mar 2018</p>
+          <p>
+            Job Description: To understand the project which is being assigned
+            and write good quality and bug free code with end to product
+            understanding. Take care of site/product performance aspects while
+            coding. Review code and ensure code changes meet quality, are bug
+            free, etc. Groom team mates to improve.
+          </p>
+          <p>
+            Accomplishments: Received 1st Prize on Hackathon (360). Received 2nd
+            Prize on Hackathon (Frenzy).
+          </p>
+          <p>Projects Handled:</p>
+          <ul className="list-disc list-inside">
+            <li>
+              Web Application (Confidential). Provides blue collar service in
+              different cities. Technologies: NodeJS (Express), JavaScript,
+              HTML, CSS, CSS3, Bootstrap.
+            </li>
+            <li>
+              E-commerce (Merchant Tool) (Confidential). Complete Enterprise
+              Solution for Merchants and Ops Team to manage products, category,
+              catalogue etc. Technologies: AngularJS (MVC), HTML, CSS, CSS3.
+            </li>
+            <li>
+              People / Talent Engagement Platform (www.peoplehum.com). Complete
+              Enterprise Solution for HR and Employee to engage with their other
+              colleagues and company etc. Technologies: AngularJS (MVC), NodeJS
+              (Express), HTML, CSS, CSS3.
+            </li>
+          </ul>
+        </div>
+
+        <div className="pl-4 border-l-4 border-primary mb-4">
+          <h3 className="text-xl font-bold">
+            Software Development Engineer, Commonfloor.com
+          </h3>
+          <p className="italic">Apr 2013 - Apr 2016</p>
+          <p>
+            Job Description: Write good quality and bug free code with end to
+            end product understanding. To drive the UI architecture and make
+            your team members learn along the way. Take care of site/product
+            performance aspects while coding. Review code and ensure code
+            changes meet quality, are bug free, etc. Ensure enough logging is
+            present for debugging any use cases.
+          </p>
+          <p>
+            Accomplishments: Received Best Foot Forward Award. Received Rising
+            Star Award. Received 2nd Prize on Hackathon (ABCD).
+          </p>
+          <p>Projects Handled:</p>
+          <ul className="list-disc list-inside">
+            <li>
+              Web Application Commonfloor Groups. Provides you the platform and
+              tools to bring all residents together to build a happy community.
+              Technologies: PHP (Zend Framework), JavaScript, HTML, CSS, CSS3,
+              Bootstrap.
+            </li>
+            <li>
+              Mobile App for Commonfloor Groups. Built from Scratch CommonFloor
+              Groups functionality on mobile devices the application is built on
+              the cordova platform (Hybrid). Technologies: AngularJS (MVC),
+              HTML, CSS, CSS3. Platforms: iOS, Android.
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold text-primary">Education</h2>
+        <p>Bhilai Institute of Technology, Durg, Chhattisgarh</p>
+        <p>Bachelor’s of Engineering — 2008 - 2012</p>
+        <p>
+          Academic Project: "E-Panorama" (PHP, HTML, JavaScript, CSS, MySQL)
+        </p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold text-primary">Skills</h2>
+        <p>Framework: Angular, Ionic, Zend</p>
+        <p>Backend: PHP, NodeJS</p>
+        <p>DBMS Package: MySQL, Mongo, Redis</p>
+        <p>
+          Front End: HTML, CSS, JavaScript, Flash, CSS3, JQuery, HTML5, SASS,
+          LESS
+        </p>
+        <p>Task Manager: Gulp</p>
+        <p>Operating Systems: Windows-Series, OSX, Ubuntu</p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold text-primary">Interests</h2>
+        <p>Virtual Gaming, Cricket, Carom</p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold text-primary">Activities</h2>
+        <p>
+          Active member of college level sports and in organizing tech-fest.
+        </p>
+        <p>Participated in robotics in 1st yr.2008</p>
+        <p>
+          Taken part in scout & guide in school and achieved a rank of 3rd
+          Sopan.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
